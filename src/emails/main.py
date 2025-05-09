f2d6dab688adeaf446497acc764d42778f524d5b
Wishcle <cpmqaq@gmail.com>
2025-05-09 11:36:24 -0400

    ruff: add ANN ruleset
    This ruleset enforces type annotations. As a result, mypy began checking
    the newly-typed functions, so this commit includes some changes to make
    mypy happy too.

--- src/emails/main.py
+++ src/emails/main.py
@@ -8,17 +8,17 @@ from tqdm import tqdm
 from emails.mbox_reader import MboxReader
 
 
-def main():
+def main() -> None:
     print_most_common_senders()
     print_names_and_emails_from_senders()
 
 
-def print_most_common_senders():
+def print_most_common_senders() -> None:
     # print("opening mbox file")
     # mbox = mailbox.mbox("/mnt/c/Users/cpmqa/Downloads/Unread-001.mbox")
 
     print("counting senders")
-    sender_counts = Counter()
+    sender_counts: Counter[str] = Counter()
     with MboxReader("data/Unread-001.mbox") as mbox:
         with tqdm(total=39629) as progress:
             for message in mbox:
@@ -42,14 +42,14 @@ def print_most_common_senders():
             f.write(f"[{count}] {sender}\n")
 
 
-def print_names_and_emails_from_senders():
+def print_names_and_emails_from_senders() -> None:
     senders_file = Path("out/senders.txt")
     assert senders_file.exists()
 
     with open(senders_file, "r") as f:
         lines = [line.strip() for line in f.readlines()]
 
-    email_counts = Counter()
+    email_counts: Counter[str] = Counter()
     p1 = re.compile(r"^\[(\d+)\].*<(.*)>")
     p2 = re.compile(r"^\[(\d+)\].* (.*)")
 
--- src/emails/mbox_reader.py
+++ src/emails/mbox_reader.py
@@ -1,25 +1,29 @@
+from __future__ import annotations
+
 import email
+from email.message import Message
 from email.policy import default
+from typing import Generator
 
 
 # https://stackoverflow.com/a/59682472/6890681
 class MboxReader:
-    def __init__(self, filename):
-        self.handle = open(filename, "rb")
+    def __init__(self, mbox_path: str) -> None:
+        self.handle = open(mbox_path, "rb")
         assert self.handle.readline().startswith(b"From ")
 
-    def __enter__(self):
+    def __enter__(self) -> MboxReader:
         return self
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
+    def __exit__(self, _exc_type, _exc_value, _exc_traceback) -> None:  # noqa: ANN001
         self.handle.close()
 
-    def __iter__(self):
-        lines = []
+    def __iter__(self) -> Generator[Message]:
+        lines: list[bytes] = []
         while True:
             line = self.handle.readline()
             if line == b"" or line.startswith(b"From "):
-                yield email.message_from_bytes(b"".join(lines), policy=default)
+                yield email.message_from_bytes(b"".join(lines), policy=default)  # type: ignore[arg-type]
                 if line == b"":
                     break
                 lines = []
